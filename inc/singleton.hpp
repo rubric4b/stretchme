caa9eae4bc73ee1cea1a12ce4ea1532f8f30944b
Grayish <hobbangz5@gmail.com>
2016-03-29 21:47:43 +0900

    add singleton templete

---
+++ inc/singleton.hpp
@@ -0,0 +1,47 @@
+//
+// Created by hobbang5 on 2016-03-24.
+//
+
+#ifndef STRETCHME_SINGLETON_H
+#define STRETCHME_SINGLETON_H
+
+template <typename T>
+class Singleton
+{
+public:
+    static T * get_InstancePtr() {
+        if(m_instance == NULL ) {
+            m_instance = new T;
+        }
+        return m_instance;
+    };
+
+    static T & Inst() {
+        if(m_instance == NULL) {
+            m_instance = new T;
+        }
+        return *m_instance;
+    };
+
+    static void release_Instance()
+    {
+        if(m_instance) {
+            delete m_instance;
+            m_instance = NULL;
+        }
+    };
+
+private:
+    static T *m_instance;
+
+protected:
+    Singleton() { };
+    virtual ~Singleton() { release_Instance(); };
+
+};
+
+template<typename T>
+T* Singleton<T>::m_instance = 0;
+
+
+#endif //STRETCHME_SINGLETON_H
